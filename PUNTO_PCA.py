5998c5168ed83e65aeda08037aac9c11b9960bdd
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-10-28 10:21:58 -0500

    Se corrigio error conceptual sobre matriz de cov
    Pensaba que la cantidad de filas que debia tener debia ser igual a la cantidad de datos, pero en realidad debe ser igual a la cantidad de categorias de los datos

--- PUNTO_PCA.py
+++ PUNTO_PCA.py
@@ -22,3 +22,22 @@ for l in atos:     #CICLO QUE RECORRE EL ARCHIVO DE DATOS Y ALMACENA SUS DATOS E
         else:
             dat[contador,i]=l[i]
     contador=contador+1
+  
+#FUNCION PARA CALCULAR LA MATRIZ DE COVARIANZA 
+def covarianza(datos):
+    numero_columnas=np.shape(datos)[1]
+
+    cantidad_datos=np.shape(datos)[0]
+
+    matriz=np.ones([numero_columnas,numero_columnas])
+    for i in range(numero_columnas):
+        for j in range(numero_columnas):
+            promedio_i=np.mean(datos[:,i])
+            promedio_j=np.mean(datos[:,j])
+            matriz[i,j]=(np.sum((datos[:,i]-promedio_i)*(datos[:,j]-promedio_j)))/(cantidad_datos-1.0)
+    return matriz
+
+auto_valores,auto_vectores=np.linalg.eig(covarianza(dat)))
+
+print("La matriz de covarianza sacada por mi cuenta da")
+print(covarianza(dat))
