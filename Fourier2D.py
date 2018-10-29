187e51b6ab5a5793e55dc3ed8861aa12d07a6004
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-10-29 14:04:31 -0500

    Se quita el ruido lo mejor posible

--- Fourier2D.py
+++ Fourier2D.py
@@ -19,3 +19,30 @@ plt.imshow(abs(Fourier_arbol),norm=LogNorm(vmin=5))
 plt.colorbar()
 plt.title('Transformada de la imagen Arbol')
 plt.savefig("CarvajalJuan_FT2D.pdf")
+
+
+#PROCESO PARA QUITAR EL RUIDO DE LA IMAGEN
+factor_c= 0.087
+factor_f= 0.087
+
+Fourier=Fourier_arbol.copy()
+
+f,c=copia.shape
+
+
+Fourier[int(f*factor_f):int(f*(1-factor_f))]=0
+
+Fourier[int(c*factor_c):int(c*(1-factor_c))]=0
+
+plt.figure()
+plt.imshow(abs(Fourier),norm=LogNorm(vmin=5))
+plt.colorbar()
+plt.title('Transformada filtrada')
+plt.savefig("CarvajalJuan_FT2D_filtrada.pdf")
+
+
+nueva=fftpack.ifft2(Fourier).real
+
+plt.figure()
+plt.imshow(nueva,plt.cm.gray)
+plt.savefig("CarvajalJuan_Imagen_filtrada.pdf")
