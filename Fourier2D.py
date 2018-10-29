2080aa1f01a194d32343aec2d8301448c19eb1ba
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-10-29 15:37:58 -0500

    Corregido el terrible filtro
    Se reemplazo el metodo de filtrado de corte directamente sobre la imagen de la transformada
    Se sabia que los puntos brillante eran el ruido y que era mejor buscar una nueva manera de quitar solo esos dos puntos en vez de cortar 
    pedazos grandes de la imagen y sacrificar resolucion

--- Fourier2D.py
+++ Fourier2D.py
@@ -7,42 +7,42 @@ from matplotlib.colors import LogNorm
 
 arbol = plt.imread('arbol.png').astype(float) #SE LEE Y ALMACENA LA IMAGEN COMO UN ARRAY DE FLOATS
 
-Fourier_arbol=fftpack.fft2(arbol) #Transformada de fourier de la imagen
+F=np.fft.fft2(arbol) #Transformada de fourier de la imagen
+Fourier_arbol=np.fft.fftshift(F)
+Frecuencias_1=np.fft.fftfreq(len(Fourier_arbol[0])) #Arreglos de las frecuencias para saber cuales son las causantes del ruido
+Frecuencias_2=np.fft.fftfreq(len(Fourier_arbol[1]))
 
 plt.figure()
-plt.imshow(Fourier_arbol)
+plt.imshow(arbol)
 plt.title("Imagen original")
 plt.savefig("Original.png")
 
 plt.figure()
-plt.imshow(abs(Fourier_arbol),norm=LogNorm(vmin=5))
+plt.imshow(np.log10(abs(Fourier_arbol)), cmap="gray")
 plt.colorbar()
 plt.title('Transformada de la imagen Arbol')
 plt.savefig("CarvajalJuan_FT2D.pdf")
 
-
-#PROCESO PARA QUITAR EL RUIDO DE LA IMAGEN
-factor_c= 0.087
-factor_f= 0.087
-
 Fourier=Fourier_arbol.copy()
-
-f,c=copia.shape
-
-
-Fourier[int(f*factor_f):int(f*(1-factor_f))]=0
-
-Fourier[int(c*factor_c):int(c*(1-factor_c))]=0
-
+#Al graficar abs(Fourier_arbol) vs Frecuencias_1 y Frecuencias_2 observé que existen cuetro picos que representan el ruido periodico
+#Ese ruido se ve en la imagen de la transformada como esos dos puntos blancos en los cuadrantes 2 y 4 
+#Para quitar esos picos debo recorrer la matriz de la transformada de fourier de la imagen y establecer unas condiciones 
+#Las condicones para quitar estos picos fueron resultado de prueba y error, sabia que debian tener menos de 5000 de magnitud pero no sabia muy bien donde cortarlos en la parte de abajo
+for i in range(np.shape(Fourier_arbol)[0]):
+  for j in range(np.shape(Fourier_arbol)[1]):
+    if(abs(Fourier[i,j])>4100 and abs(Fourier[i,j])<5000):
+      Fourier[i,j]=0.1  #Pongo 0.1 en vez de 0.0 para que cuando saque el logaritmo no me salga error (la imagen sale igual de bien)
+      
 plt.figure()
-plt.imshow(abs(Fourier),norm=LogNorm(vmin=5))
+plt.imshow(np.log10(abs(Fourier)), cmap="gray")
 plt.colorbar()
-plt.title('Transformada filtrada')
+plt.title("Transformada filtrada")
 plt.savefig("CarvajalJuan_FT2D_filtrada.pdf")
 
-
-nueva=fftpack.ifft2(Fourier).real
+     
+nueva=np.fft.ifft2(Fourier)
 
 plt.figure()
-plt.imshow(nueva,plt.cm.gray)
+plt.imshow(abs(nueva),cmap="gray")
+plt.title("Imagen sin ruido")
 plt.savefig("CarvajalJuan_Imagen_filtrada.pdf")
