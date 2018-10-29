75fc3d48a73009e03cc61d1410dad152082501e6
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-10-29 13:54:58 -0500

    Se calcula la transformada de la imagen

--- Fourier2D.py
+++ Fourier2D.py
@@ -5,4 +5,17 @@ from scipy import ndimage
 from PIL import Image
 from matplotlib.colors import LogNorm
 
-im = plt.imread('arbol.png').astype(float) #SE LEE Y ALMACENA LA IMAGEN COMO UN ARRAY DE FLOATS
+arbol = plt.imread('arbol.png').astype(float) #SE LEE Y ALMACENA LA IMAGEN COMO UN ARRAY DE FLOATS
+
+Fourier_arbol=fftpack.fft2(arbol) #Transformada de fourier de la imagen
+
+plt.figure()
+plt.imshow(Fourier_arbol)
+plt.title("Imagen original")
+plt.savefig("Original.png")
+
+plt.figure()
+plt.imshow(abs(Fourier_arbol),norm=LogNorm(vmin=5))
+plt.colorbar()
+plt.title('Transformada de la imagen Arbol')
+plt.savefig("CarvajalJuan_FT2D.pdf")
